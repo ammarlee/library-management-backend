148e803ff6ab34d3894734d29ff83445b6416821
ammarlee <ammarlee16@gmail.com>
2026-09-14 15:20:08 +0300

    fix the port issue

--- src/main.ts
+++ src/main.ts
@@ -56,7 +56,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   const port = configService.get<number>('port') ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
